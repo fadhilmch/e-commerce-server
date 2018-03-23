586fdd535d8cbdce6e53ca0e6538b2eca2be62bb
Fadhilmch <fadhil.mochammad1095@gmail.com>
2018-03-24 00:01:13 +0700

    updated

--- controllers/items.controller.js
+++ controllers/items.controller.js
@@ -1,7 +1,28 @@
 const Item = require('../models/items.model');
 
 module.exports = {
-  findAll: (req, res) => {
+  uploadFile: (req, res) => {
+        Item.create({
+                name: req.body.name,
+                imageUrl: req.file.cloudStoragePublicUrl,
+                description: req.body.description,
+                stock: req.body.stock,
+                price: req.body.price,
+            }, (err, newpost) => {
+                if (err) {
+                    return res.status(400).json({
+                        message: `create post erorr ${err}`,
+                        data: {}
+                    })
+                } else {
+                    res.status(200).json({
+                        message: `post success`,
+                        data: newpost
+                    })
+                }
+            })
+    },
+    findAll: (req, res) => {
     Item.find()
       .exec()
       .then(data => {
--- routes/items.js
+++ routes/items.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const {findAll, create, destroy, update, findById} = require('../controllers/items.controller');
+const {uploadFile, findAll, create, destroy, update, findById} = require('../controllers/items.controller');
+const {sendUploadToGCS} = require('../middleware/uploadGCS');
+const multer = require('multer');
+
+const upload = multer({
+   storage  : multer.memoryStorage(),
+   limits   : {
+     fileSize: 10*1024*1024
+  }
+ })
 
 router.get('/', findAll);
 router.post('/', create);
+router.post('/upload',upload.single('item'),sendUploadToGCS,uploadFile)
 
 router.get('/:id', findById);
 router.put('/:id', update);
